433ef17949a75b748e7529b57cfe665fbb0d62e7
zachkay <mrzr97@gmail.com>
2021-05-03 10:47:37 +0800

    2nd commit

--- lib/screen/screen2.dart
+++ lib/screen/screen2.dart
@@ -1,25 +1,17 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_login/models/input.dart';
+import '../models/CalculationDetail.dart';
+import './screen3.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-class CalcScreen2 extends StatelessWidget {
-  const CalcScreen2({Key key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text('CalcForms'),
-      ),
-      body: CalcForm2(), // We'll add this in a bit
-    );
-  }
-}
+import 'package:http/http.dart' as http;
 
 class CalcForm2 extends StatefulWidget {
-  CalcForm2({Key key}) : super(key: key);
+  final CalculationDetail calculationDetail;
+  CalcForm2(this.calculationDetail);
+
   @override
   _CalcForm2State createState() => _CalcForm2State();
 }
@@ -27,234 +19,238 @@ class CalcForm2 extends StatefulWidget {
 class _CalcForm2State extends State<CalcForm2> {
   final _formKey = GlobalKey<FormState>();
 
-  CardDetails _cardDetails = new CardDetails();
-  String expiryMonth;
-  String expiryYear;
-  Address _paymentAddress = new Address();
-
   bool loading = false;
 
-  final List yearsList = List.generate(12, (int index) => index + 2020);
   Map<String, bool> touched = {
     "locAField": false,
   };
 
-  final TextEditingController tfLocA = TextEditingController();
-  final TextEditingController tfLocB = TextEditingController();
+  final TextEditingController tfCPhase = TextEditingController();
+  final TextEditingController tfAllowedVD = TextEditingController();
   final TextEditingController tfDist = TextEditingController();
   final TextEditingController tfCurrentLoad = TextEditingController();
 
-  
+  String selectedBreaker = "";
+  // List data = List();
+  Map<String, dynamic> data = new Map<String, dynamic>();
+  List breakerData = [];
+  List cphaseData = [];
+  List ctypeData = [];
+  List cizData = [];
+  List cvdData = [];
+  List cpriceData = [];
+
+  Future getAllData() async {
+    var response = await http.get("http://10.0.2.2/budee/call_data.php",
+        headers: {"Accept": "application/json"});
+    var jsonBody = response.body;
+    var jsonData = json.decode(jsonBody);
+
+    setState(() {
+      data = jsonData;
+      breakerData = data["breaker"];
+      cphaseData = data["cphase"];
+      ctypeData = data["ctype"];
+      cizData = data["ciz"];
+      cvdData = data["cvd"];
+      cpriceData = data["cprice"];
+    });
+
+    print(jsonData);
+  }
   // double estDist = dist * 0.15;
   // double dist = double.parse(tfDist.text);
 
   @override
+  void initState() {
+    super.initState();
+    print(widget.calculationDetail.locA);
+    getAllData();
+  }
+
+  toScreen3() {
+    widget.calculationDetail.cPhase = '';
+    widget.calculationDetail.allowedVD = double.parse('3');
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => Screen3(widget.calculationDetail),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Form(
-        key: _formKey,
-        child: ListView(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(children: <Widget>[
-                Container(
-                  child: Text(
-                    'CABLE TYPE SELECTION',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
-                DropdownButtonFormField<String>(
-                  onSaved: (val) => _cardDetails.expiryMonth = val,
-                  value: expiryMonth,
-                  items: [
-                    'SINGLE PHASE',
-                    'THREE PHASE'
-                  ].map<DropdownMenuItem<String>>(
-                    (String val) {
-                      return DropdownMenuItem(
-                        child: Text(val),
-                        value: val,
-                      );
-                    },
-                  ).toList(),
-                  onChanged: (val) {
-                    setState(() {
-                      expiryMonth = val;
-                    });
-                  },
-                  decoration: InputDecoration(
-                    labelText: 'Voltage',
-                    // icon: Icon(Icons.calendar_today),
-                  ),
-                ),
-                Container(
-                  child:
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0.0,20.0,0.0,20.0),
-                        child: Text(
-                          'VOLTAGE DROP PERCENTAGE',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ),                  
-                ),
-                Container(
-                  child:
-                      Text(
-                        'Notes: Maximum voltage drop allowed based on IEEE is 8% and JKR is 4%',
-                        style: TextStyle(fontStyle: FontStyle.italic),
-                      ),
-                  ),
-                TextFormField(
-                  controller: tfLocA,
-                  onSaved: (val) => _cardDetails.cardHolderName = val,
-                  onChanged: (value) {
-                    setState(() {
-                      touched['locAField'] = true;
-                    });
-                  },
-                  decoration: InputDecoration(
-                    labelText: 'Voltage Drop Allowed',
-                    // icon: Icon(Icons.location_on),
-                  ),
-                  validator: (value) {
-                    if (value.isEmpty) return "This form value must be filled";
-                    return null;
-                  },
-                  autovalidate: touched['locAField'],
-                ),
-                DropdownButtonFormField<String>(
-                  onSaved: (val) => _cardDetails.expiryMonth = val,
-                  value: expiryMonth,
-                  items: [
-                    'INDOOR',
-                    'OUTDOOR'
-                  ].map<DropdownMenuItem<String>>(
-                    (String val) {
-                      return DropdownMenuItem(
-                        child: Text(val),
-                        value: val,
-                      );
-                    },
-                  ).toList(),
-                  onChanged: (val) {
-                    setState(() {
-                      expiryMonth = val;
-                    });
-                  },
-                  decoration: InputDecoration(
-                    labelText: 'Installation Type',
-                    // icon: Icon(Icons.calendar_today),
-                  ),
-                ),
-                DropdownButtonFormField<String>(
-                  onSaved: (val) => _cardDetails.expiryMonth = val,
-                  value: expiryMonth,
-                  items: [
-                    'PVC(Indoor)',
-                    'XLPE(Indoor)',
-                    'XLPE/SWA/PVC(Outdoor)',
-                    'PVC/SWA/PVC(Outdoor)'
-                  ].map<DropdownMenuItem<String>>(
-                    (String val) {
-                      return DropdownMenuItem(
-                        child: Text(val),
-                        value: val,
-                      );
-                    },
-                  ).toList(),
-                  onChanged: (val) {
-                    setState(() {
-                      expiryMonth = val;
-                    });
-                  },
-                  decoration: InputDecoration(
-                    labelText: 'Cable Type',
-                    // icon: Icon(Icons.calendar_today),
-                  ),
-                ),
-                DropdownButtonFormField<String>(
-                  onSaved: (val) => _cardDetails.expiryMonth = val,
-                  value: expiryMonth,
-                  items: [
-                    '1',
-                    '2',
-                    '3',
-                    '4'
-                  ].map<DropdownMenuItem<String>>(
-                    (String val) {
-                      return DropdownMenuItem(
-                        child: Text(val),
-                        value: val,
-                      );
-                    },
-                  ).toList(),
-                  onChanged: (val) {
-                    setState(() {
-                      expiryMonth = val;
-                    });
-                  },
-                  decoration: InputDecoration(
-                    labelText: 'Cable Quantity',
-                    // icon: Icon(Icons.calendar_today),
-                  ),
-                ),
-                
-                Container(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        RaisedButton(
-                          child: Text('Reset'),
-                          color: Colors.amber,
-                          textColor: Colors.black,
-                          onPressed: () {
-                            _formKey.currentState.reset();
-                            tfLocA.clear();
-                            tfLocB.clear();
-                            tfDist.clear();
-                            tfCurrentLoad.clear();
-                          },
-                        ),
-                        RaisedButton(
-                          child: loading
-                              ? SpinKitWave(
-                                  color: Colors.white,
-                                  size: 15.0,
-                                )
-                              : Text('Next'),
-                          color: Colors.amber,
-                          textColor: Colors.black,
-                          onPressed: () {
-                            if (_formKey.currentState.validate()) {
-                              setState(() {
-                                loading = true;
-                              });
-                              _formKey.currentState.save();
-                              Timer(Duration(seconds: 1), () {
-                                Payment payment = new Payment(
-                                    address: _paymentAddress,
-                                    cardDetails: _cardDetails);
-                                setState(() {
-                                  loading = false;
-                                });
-                                final snackBar =
-                                    SnackBar(content: Text('Calculating...'));
-                                Scaffold.of(context).showSnackBar(snackBar);
-                                print('Saved');
-                              });
-                            }
-                          },
-                        ),
-                      ],
+    //getAllBreaker();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text('Screen2'),
+      ),
+      body: Container(
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(children: <Widget>[
+                  Container(
+                    child: Text(
+                      'CABLE TYPE SELECTION',
+                      style: TextStyle(fontWeight: FontWeight.bold),
                     ),
                   ),
-                ),
-              ]),
-            ), // we will work in here
-          ],
+                  DropdownButtonFormField(
+                    //onSaved: (val) => _cardDetails.expiryMonth = val,
+                    //value: selectedBreaker,
+                    decoration: InputDecoration(
+                      labelText: 'Cable Phase',
+                      // icon: Icon(Icons.calendar_today),
+                    ),
+                    items: cphaseData.map(
+                      (list) {
+                        return DropdownMenuItem(
+                          child: Text(list['phase']),
+                          value: list['phase'],
+                        );
+                      },
+                    ).toList(),
+                    onChanged: (val) {
+                      setState(() {
+                        //selectedBreaker = val;
+                      });
+                    },
+                  ),
+                  Container(
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 20.0),
+                      child: Text(
+                        'VOLTAGE DROP PERCENTAGE',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Text(
+                      'Notes: Maximum voltage drop allowed based on IEEE is 8% and JKR is 4%',
+                      style: TextStyle(fontStyle: FontStyle.italic),
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Voltage Drop Allowed',
+                      // icon: Icon(Icons.location_on),
+                    ),
+                    controller: tfLocA,
+                    // onSaved: (val) => _cardDetails.cardHolderName = val,
+                    onChanged: (value) {
+                      setState(() {
+                        touched['locAField'] = true;
+                      });
+                    },
+                    validator: (value) {
+                      if (value.isEmpty)
+                        return "This form value must be filled";
+                      return null;
+                    },
+                    autovalidate: touched['locAField'],
+                  ),
+                  DropdownButtonFormField(
+                    //onSaved: (val) => _cardDetails.expiryMonth = val,
+                    //value: selectedBreaker,
+                    decoration: InputDecoration(
+                      labelText: 'Installation Type',
+                      // icon: Icon(Icons.calendar_today),
+                    ),
+                    items: <String>['INDOOR', 'OUTDOOR']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        child: Text(value),
+                        value: value,
+                      );
+                    }).toList(),
+                    onChanged: (val) {
+                      setState(() {
+                        //selectedBreaker = val;
+                      });
+                    },
+                  ),
+                  DropdownButtonFormField(
+                    //onSaved: (val) => _cardDetails.expiryMonth = val,
+                    //value: selectedBreaker,
+                    decoration: InputDecoration(
+                      labelText: 'Cable Type',
+                      // icon: Icon(Icons.calendar_today),
+                    ),
+                    items: cphaseData.map(
+                      (list) {
+                        return DropdownMenuItem(
+                          child: Text(list['phase']),
+                          value: list['phase'],
+                        );
+                      },
+                    ).toList(),
+                    onChanged: (val) {
+                      setState(() {
+                        //selectedBreaker = val;
+                      });
+                    },
+                  ),
+                  Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          RaisedButton(
+                            child: Text('Reset'),
+                            color: Colors.amber,
+                            textColor: Colors.black,
+                            onPressed: () {
+                              _formKey.currentState.reset();
+                              tfLocA.clear();
+                              tfLocB.clear();
+                              tfDist.clear();
+                              tfCurrentLoad.clear();
+                            },
+                          ),
+                          RaisedButton(
+                            child: loading
+                                ? SpinKitWave(
+                                    color: Colors.white,
+                                    size: 15.0,
+                                  )
+                                : Text('Next'),
+                            color: Colors.amber,
+                            textColor: Colors.black,
+                            onPressed: () {
+                              if (_formKey.currentState.validate()) {
+                                setState(() {
+                                  loading = true;
+                                });
+                                _formKey.currentState.save();
+                                Timer(Duration(seconds: 1), () {
+                                  setState(() {
+                                    loading = false;
+                                  });
+                                  final snackBar =
+                                      SnackBar(content: Text('Calculating...'));
+                                  Scaffold.of(context).showSnackBar(snackBar);
+                                  print('Saved');
+                                });
+                              }
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ]),
+              ), // we will work in here
+            ],
+          ),
         ),
       ),
     );
